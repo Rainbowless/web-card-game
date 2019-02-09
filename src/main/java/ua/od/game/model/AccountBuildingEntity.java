c59f8ec19669d8e8b93e48e444ca6bb8270b3700
Rainbowless <itsokaytobegeek@gmail.com>
2019-02-09 13:08:43 +0200

    some changes

--- src/main/java/ua/od/game/model/AccountBuildingEntity.java
+++ src/main/java/ua/od/game/model/AccountBuildingEntity.java
@@ -1,31 +1,6 @@
 package ua.od.game.model;
 
 public class AccountBuildingEntity {
-    private Integer id;
     private Integer buildingId;
     private Float amount;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getBuildingId() {
-        return buildingId;
-    }
-
-    public void setBuildingId(Integer buildingId) {
-        this.buildingId = buildingId;
-    }
-
-    public Float getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Float amount) {
-        this.amount = amount;
-    }
 }
