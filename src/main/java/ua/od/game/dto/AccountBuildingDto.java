b3a2c7ecee2c0c94cf5fa713f78e3e6cb2f8c7a7
Rainbowless <itsokaytobegeek@gmail.com>
2019-02-09 13:35:28 +0200

    AccountBuilding Service and Controller added

--- src/main/java/ua/od/game/dto/AccountBuildingDto.java
+++ src/main/java/ua/od/game/dto/AccountBuildingDto.java
@@ -3,4 +3,12 @@ package ua.od.game.dto;
 public class AccountBuildingDto {
     private Integer buildingId;
     private Float amount;
+
+    public Integer getBuildingId() { return buildingId; }
+
+    public void setBuildingId(Integer buildingId) { this.buildingId = buildingId; }
+
+    public Float getAmount() { return amount; }
+
+    public void setAmount(Float amount) { this.amount = amount; }
 }
